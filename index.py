185ea05611fb93a722c40eaa4ba00720f69d982e
aliziafati <aliziafati@gmail.com>
2025-11-23 20:49:45 +0330

    Initial Commit

---
+++ index.py
@@ -0,0 +1,15 @@
+name = "alizia"
+
+def call_name(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(call_name(name))
+
+
+num = 1234567
+def call_num(num):
+    return f"Welcome to the municipality of {num}!"
+
+if __name__ == "__main__":
+    print(call_num(num))
