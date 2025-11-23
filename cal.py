1ae80dfc6b62365675b35e6ac931259e74c67136
aliziafati <aliziafati@gmail.com>
2025-11-23 21:03:44 +0330

    App cal diff

--- cal.py
+++ cal.py
@@ -6,3 +6,8 @@ def sum_nums(num1, num2):
 
 sum_result = sum_nums(num1, num2)
 print("Sum:", sum_result)
+
+def diff_nums(num1, num2):
+    return num1 - num2
+diff_result = diff_nums(num1, num2)
+print("Difference:", diff_result)
